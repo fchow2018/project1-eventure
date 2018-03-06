24ce8dc41170646b3dcc8cf99c87524bcbfde5fc
Manjilan <nakarmimanjila@yahoo.com>
2018-03-05 16:16:46 -0800

    js changes

--- app/assets/javascripts/ajx.js
+++ app/assets/javascripts/ajx.js
@@ -2,10 +2,10 @@
 
 // var token = 'GGAQ2BUKIRGJMZMU55YZ';
 var token = 'DQVX342GH577YJBOWRDB';
-// let fullURL = `https://www.eventbriteapi.com/v3/events/search/?token= ${token}`;
-let fullURL = "https://www.eventbriteapi.com/v3/events/search/?token=DQVX342GH577YJBOWRDB"
-let barca = "https://www.eventbriteapi.com/v3/events/search/?sort_by=distance&location.address=barcelona%2C+spain&start_date.keyword=this_week&token=DQVX342GH577YJBOWRDB"
-let sf = `https://www.eventbriteapi.com/v3/events/search/?sort_by=distance&location.address=san+francisco&start_date.keyword=this_week&token=${token}`
+
+var fullURL = "https://www.eventbriteapi.com/v3/events/search/?token=DQVX342GH577YJBOWRDB"
+var barca = "https://www.eventbriteapi.com/v3/events/search/?sort_by=distance&location.address=barcelona%2C+spain&start_date.keyword=this_week&token=DQVX342GH577YJBOWRDB"
+var sf = "https://www.eventbriteapi.com/v3/events/search/?sort_by=distance&location.address=san+francisco&start_date.keyword=this_week&token="+ token
 
 //Halim tab idea
 var tab=[]
@@ -38,43 +38,10 @@ let displayEvents = function(json) {
   json.events.forEach(function(event, i) {
   // console.log(event);
 
-      var eventInstance = `
-        <div class="event-section">
-        <div class="event_item">
-          <div class="row event-row">
-            <p class="title sidebyside">${event.name.text}</p>
-          </div>
-        </div>
-        </div>
-         `;
+      var eventInstance = "
+        <div class='event-section'><div class='event_item'><div class='row event-row'><p class='title sidebyside'> "+ event.name.text +"</p></div></div></div>";
 
     $("#info").append(eventInstance);
   });
 
-}; //end displayEvents
-
-// events_list = [];
-// function save(index){
-//   console.log(tab[index].description.text);
-//   events_list.push(tab[index]);
-//
-//
-//
-//   //Halim internal AJAX
-//   $.ajax({
-//     url: '/events',
-//     method:'POST',
-//     data: {
-//     event_name: tab[index].name.text,
-//     event_description: tab[index].description.text
-//
-//     },
-//     success: function(data){
-//       $('#event_list').append(`<p class="sidebyside">${data.event_name}</p> <button class="btn btn-floating sidebyside">X</button>`)
-//     },
-//     error:function(){
-//       alert('not added')
-//     }
-//   });
-
-// };
+};
